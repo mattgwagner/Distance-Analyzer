8510b8359ef72df9c4e1f1b6b4d93c2983c0ce1b
Matt Wagner <mattgwagner@gmail.com>
2017-04-13 20:59:12 -0400

    Fix endpoint, add tags

--- Load.ps1
+++ Load.ps1
@@ -2,13 +2,14 @@ $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $Soldiers = Get-Content Roster.csv | ConvertFrom-Csv
 
-$Endpoint = "http://localhost:59400/New"
+$Endpoint = "http://localhost:59400/Nodes/New"
 
 foreach($Soldier in $Soldiers)
 {
 	$Node = @{
 		Description = $Soldier.Name;
 		Address = "$($Soldier.'Home Address'), $($Soldier.'Home City') $($Soldier.'Zip Code')";
+		Tags = @($Soldier.UIC, $Soldier.Unit, $Soldiers.Rank)
 	}
 
 	Invoke-RestMethod -Method Post -Uri $Endpoint -Body $Node
